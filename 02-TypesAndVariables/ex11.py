7f38eb193c6b1b9be80858756d8af9769dca18e1
s-013-24 <s-013-24@gumiforest.ae.krakow.pl>
2023-10-11 16:30:37 +0200

    My first Python programs from textbook

---
+++ 02-TypesAndVariables/ex11.py
@@ -0,0 +1,9 @@
+father = 5000
+mother = 6000
+num = 4
+total_income = father + mother
+income = total_income / num
+print(f"Enter father’s income: {father}")
+print(f"Enter mother’s income: {mother}")
+print(f"Total income: {total_income}")
+print(f"Income per person: {income}")
---
+++ 02-TypesAndVariables/name.py
@@ -0,0 +1,11 @@
+
+imie = "Karolina"
+age = 19
+
+height = 166
+
+years = age + 6
+
+print(f"My name is {imie} I am {age} years old, and my height is {height} cm. In 6 years I will be {years} years old.")
+print(x.fromat(name, age, height, years))
+
